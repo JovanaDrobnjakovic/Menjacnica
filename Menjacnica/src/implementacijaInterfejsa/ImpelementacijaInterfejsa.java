f92231a4cc2df6bdbd35cb52e299fe2971f0b8d3
JovanaDrobnjakovic <drobnjakovic2216@fon.bg.ac.rs>
2018-03-15 09:42:04 +0100

    Izmenjena metoda dodajValutu u grani master

--- Menjacnica/src/implementacijaInterfejsa/ImpelementacijaInterfejsa.java
+++ Menjacnica/src/implementacijaInterfejsa/ImpelementacijaInterfejsa.java
@@ -1,19 +1,30 @@
 package implementacijaInterfejsa;
 
+import java.util.LinkedList;
+
+import Valuta.Valuta;
 import interfejsMenjacnica.InterfejsMenjacnica;
 
 public class ImpelementacijaInterfejsa implements InterfejsMenjacnica {
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
-	@Override
 	public void dodajKurs(double prodajniKurs, double srednjiKurs, double kupovniKurs, String nazivValute) {
-		// TODO Auto-generated method stub
-		
+		if (nazivValute != null) {
+			for (int i = 0; i < valute.size(); i++) {
+				if (valute.get(i).getNazivValute().equals(nazivValute)) {
+					valute.get(i).setKupovniKurs(kupovniKurs);
+					valute.get(i).setProdajniKurs(prodajniKurs);
+					valute.get(i).setSrednjiKurs(srednjiKurs);
+				}
+			}
+		}
+
 	}
 
 	@Override
 	public void obrisiKurs(String nazivValute) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
