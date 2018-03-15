0541253bd44928c261a06036abcf7ff96fdfca9b
JovanaDrobnjakovic <drobnjakovic2216@fon.bg.ac.rs>
2018-03-15 09:31:46 +0100

    Napravljena je klasa ImplementacijaInterfejsa u kojoj su tela metoda ostala "prazna"

---
+++ Menjacnica/src/implementacijaInterfejsa/ImpelementacijaInterfejsa.java
@@ -0,0 +1,25 @@
+package implementacijaInterfejsa;
+
+import interfejsMenjacnica.InterfejsMenjacnica;
+
+public class ImpelementacijaInterfejsa implements InterfejsMenjacnica {
+
+	@Override
+	public void dodajKurs(double prodajniKurs, double srednjiKurs, double kupovniKurs, String nazivValute) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKurs(String nazivValute) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double vratiKurs(String nazivValute) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
