8365221609fdcb1e62f60296bdf31cd53cfe4414
JovanaDrobnjakovic <drobnjakovic2216@fon.bg.ac.rs>
2018-03-15 09:09:48 +0100

    Generisane su metode equals, hashCode i toString

--- Menjacnica/src/Valuta/Valuta.java
+++ Menjacnica/src/Valuta/Valuta.java
@@ -47,4 +47,54 @@ public class Valuta {
 		this.kupovniKurs = kupovniKurs;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((nazivValute == null) ? 0 : nazivValute.hashCode());
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skraceniNazivValute == null) ? 0 : skraceniNazivValute.hashCode());
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (Double.doubleToLongBits(kupovniKurs) != Double.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (nazivValute == null) {
+			if (other.nazivValute != null)
+				return false;
+		} else if (!nazivValute.equals(other.nazivValute))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (skraceniNazivValute == null) {
+			if (other.skraceniNazivValute != null)
+				return false;
+		} else if (!skraceniNazivValute.equals(other.skraceniNazivValute))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double.doubleToLongBits(other.srednjiKurs))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Valuta [nazivValute=" + nazivValute + ", skraceniNazivValute=" + skraceniNazivValute + ", prodajniKurs="
+				+ prodajniKurs + ", srednjiKurs=" + srednjiKurs + ", kupovniKurs=" + kupovniKurs + "]";
+	}
+
 }
